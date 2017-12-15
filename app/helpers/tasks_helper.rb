3e84325d242de37b9059ab8ba0e0bb8447186220
jiananWang <a0918666589@gmail.com>
2017-12-15 23:30:51 +0800

    refactoring the index.html.erb and put the function into helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,31 @@
 module TasksHelper
+    # todo_check_button用來辨識status的helper，使用在index.html.erb
+    # button_checked, button_unchecked是來裝html字串的變數
+    def todo_check_button(t)
+        if t.status == false
+            link_to check_task_path(t), method: :post do
+                button_unchecked = '<i class="fa fa-square-o" aria-hidden="true"></i>'
+                button_unchecked.html_safe
+            end
+        else
+            link_to check_task_path(t.id), method: :post do
+                button_checked = '<i class="fa fa-check-square" aria-hidden="true"></i>'
+                button_checked.html_safe
+            end
+        end
+    end
+    # todo_edit_button是edit的helper，使用在index.html.erb
+    def todo_edit_button(t)
+        link_to edit_task_path(t) do
+            edit_button = '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>'
+            edit_button.html_safe
+        end
+    end
+    # todo_delete_button是delete的helper，使用在index.html.erb
+    def todo_delete_button(t)
+        link_to task_path(t), method: :delete, date: { confirm: 'Are you sure?' } do
+            edit_button = '<i class="fa fa-times" aria-hidden="true"></i>'
+            edit_button.html_safe
+        end
+    end
 end
